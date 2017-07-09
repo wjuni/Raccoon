524788e5becb91a6055ef015f23e39b9ba7d6024
Hwijoon Lim <wjuni@kaist.ac.kr>
2017-07-09 16:26:33 +0200

    Initial Commit

---
+++ Makerbot-Lanepainter/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Makerbot-Lanepainter
+//
+//  Created by 임휘준 on 2017. 7. 9..
+//  Copyright © 2017년 임휘준. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
