62fc603ece105b1557233f7e2c5d2d1dfc4f7873
Hwijoon Lim <wjuni@kaist.ac.kr>
2017-08-11 00:05:22 +0200

    Applied Weighted Avg./Stdev./Coeff.

--- python/algorithm_v3_0810.py
+++ python/algorithm_v3_0810.py
@@ -1,19 +1,21 @@
 import cv2
 import os.path
 import numpy as np
-import matplotlib.pyplot as plt
-
 
 LINEWIDTH_DEFAULT = 0.220 # of screen width
 TARGET_FRAMESIZE = (240, 136)
 N = 8
-CONV_THRESH = 50  # change according to the screen width (30% of width is appropriate)
+CONV_THRESH = 25  # change according to the screen width (30% of width is appropriate)
 LINE_MARGIN_RATIO = 1.2
 
 LINEWIDTH_INITIAL = LINEWIDTH_DEFAULT * TARGET_FRAMESIZE[0]
 STEPS_N = TARGET_FRAMESIZE[1] / N
 
+global_linewidth_estimation = None
+
 def process(path, filename, rotate, lines):
+    global global_linewidth_estimation
+
     if not os.path.isfile(path + filename):
         return False
 
@@ -37,6 +39,9 @@ def process(path, filename, rotate, lines):
     line_center = []
     line_left = []
     line_right = []
+
+    local_max_linewidth = 0
+
     for i in range(N):
         cropped = frame_threshed[i*STEPS_N:(i+1)*STEPS_N, :]
         vec_avg = np.average(cropped, axis=0)
@@ -46,6 +51,9 @@ def process(path, filename, rotate, lines):
         # print normalized_vec
         estimated_linewidth = (np.sum(vec_avg) / 255)
         print 'linewidth', estimated_linewidth
+        if estimated_linewidth > local_max_linewidth :
+            local_max_linewidth = estimated_linewidth
+
 
         conv_filter = np.zeros(shape=(int(estimated_linewidth*1.5)))
         conv_filter[:] = -1
@@ -69,18 +77,54 @@ def process(path, filename, rotate, lines):
             left_line = max_arg - estimated_linewidth/2.*LINE_MARGIN_RATIO
             right_line = max_arg + estimated_linewidth/2.*LINE_MARGIN_RATIO
             conv[int(left_line):int(right_line)] = 0
-            line_center.append((max_arg, (i+0.5)*STEPS_N, (max_v-CONV_THRESH)/10))
+            line_center.append((max_arg, (i+0.5)*STEPS_N, (max_v-CONV_THRESH)/10, estimated_linewidth))
             line_right.append((max_arg+estimated_linewidth/2., (i + 0.5) * STEPS_N, 2))
             line_left.append((max_arg-estimated_linewidth/2., (i + 0.5) * STEPS_N, 2))
 
     # print line_center
     # print LINEWIDTH_INITIAL
+    center_xsample = []
+    center_ysample = []
+    center_weights = []
     for item in line_center:
         cv2.circle(im, (int(item[0]), int(item[1])), 2, (0, 0, 255), thickness=int(item[2]))
-    for item in line_left:
-        cv2.circle(im, (int(item[0]), int(item[1])), 2, (255, 0, 0), thickness=int(item[2]))
-    for item in line_right:
-        cv2.circle(im, (int(item[0]), int(item[1])), 2, (255, 0, 0), thickness=int(item[2]))
+        center_xsample.append(item[0])
+        center_ysample.append(item[1])
+        center_weights.append(item[2]*((estimated_linewidth/local_max_linewidth)**2))
+
+    if global_linewidth_estimation is None:
+        global_linewidth_estimation = local_max_linewidth
+    else:
+        global_linewidth_estimation = 0.9 * global_linewidth_estimation + 0.1 * local_max_linewidth
+    print global_linewidth_estimation
+
+    try:
+        xsample = np.array(center_xsample)
+        ysample = np.array(center_ysample)
+        weights = np.array(center_weights)
+        xavg = np.average(xsample, weights=weights)
+        yavg = np.average(ysample, weights=weights)
+        xstd = np.sqrt(np.sum(weights * (xsample-xavg) **2) / np.sum(weights))
+        ystd = np.sqrt(np.sum(weights * (ysample-yavg) **2) / np.sum(weights))
+        cov = np.sum(weights * (xsample - xavg) * (ysample - yavg)) / np.sum(weights)
+        corr = cov / (xstd * ystd)
+        # print corr, np.corrcoef(xsample, ysample)[0, 1]
+        betahat = corr * xstd / ystd
+        alphahat = xavg - yavg * betahat
+        print betahat, alphahat
+        cv2.line(im,
+                 (int(alphahat), 0),
+                 (int(alphahat + betahat * height), height), (255, 0, 255), 2)
+        cv2.line(im,
+                 (int(alphahat + global_linewidth_estimation / 2), 0),
+                 (int(alphahat + betahat * height + global_linewidth_estimation / 2), height), (0, 255, 0), 2)
+        cv2.line(im,
+                 (int(alphahat - global_linewidth_estimation / 2), 0),
+                 (int(alphahat + betahat * height - global_linewidth_estimation / 2), height), (0, 255, 0), 2)
+    except:
+        print ("To minimal")
+
+    # alpha = np.average(xsample) - average
 
     #filtered = cv2.bitwise_and(im, im, mask=frame_threshed)
 
